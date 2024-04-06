9818c645d61e1be8c4b4496fd8b2a21d3b0a2491
serhiisezonchyk <sergey040901@gmail.com>
2024-04-06 11:24:39 +0300

    feat: add error boundary (using react-error-boundary)

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import '@fontsource/roboto/700.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Provider } from 'react-redux';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
@@ -14,10 +15,22 @@ import HomePage from './pages/home/HomePage.tsx';
 import MoviesPage from './pages/movies/MoviesPage.tsx';
 import store from './store/index.ts';
 
+function fallbackRender({ error, resetErrorBoundary }: { error: Error; resetErrorBoundary(): void }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+      <pre style={{ color: 'red' }}>{error.stack}</pre>
+    </div>
+  );
+}
+
 const AppEntrypoint = () => {
   return (
     <Provider store={store}>
-      <App />
+      <ErrorBoundary fallbackRender={fallbackRender} onError={(error, _) => alert(error)}>
+        <App />
+      </ErrorBoundary>
     </Provider>
   );
 };
