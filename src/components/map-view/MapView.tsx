af2677451167cdc3d3b88e8475373d4c631d1947
serhiisezonchyk <sergey040901@gmail.com>
2024-04-06 10:45:05 +0300

    feat(About/MapView): add map using portals

---
+++ src/components/map-view/MapView.tsx
@@ -0,0 +1,35 @@
+import { Favorite } from '@mui/icons-material';
+import { Box, Container, Typography } from '@mui/material';
+import { Map } from 'leaflet';
+import { useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { addPopupToMapWidget, createMapWidget } from './mapWidget';
+const Greeting = () => {
+  return (
+    <Box>
+      <Typography>Greetings from Ukraine.</Typography>
+      <Favorite sx={{color:'#0056B9'}}/>
+      <Favorite sx={{color:'#FFD800'}}/>
+    </Box>
+  );
+};
+const MapView = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<Map | null>(null);
+  const [popupContainer, setPopupContainer] = useState<HTMLElement | null>(null);
+  useEffect(() => {
+    if (mapRef.current === null) {
+      const map = createMapWidget(containerRef.current!);
+      mapRef.current = map;
+      const popupDiv = addPopupToMapWidget(map);
+      setPopupContainer(popupDiv);
+    }
+  }, []);
+  return (
+    <Container ref={containerRef} sx={{ width: 800, height: 500, my: 2 }}>
+      {popupContainer !== null && createPortal(<Greeting />, popupContainer)}
+    </Container>
+  );
+};
+
+export default MapView;
--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,12 +1,14 @@
 import { Container } from '@mui/material';
 import CountDownText from '../../components/count-down-text/CountDownText';
 import CounDownVideo from '../../components/count-down-video/CounDownVideo';
+import MapView from '../../components/map-view/MapView';
 
 const AboutPage = () => {
   return (
     <Container sx={{ py: 8 }} maxWidth="md">
       <CountDownText />
       <CounDownVideo />
+      <MapView/>
     </Container>
   );
 };
